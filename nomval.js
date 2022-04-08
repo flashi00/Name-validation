2a8c105cd622c9d9370290c6f71404b281bd662f
KABILA Anas <93036687+flashi00@users.noreply.github.com>
2022-04-08 18:49:18 +0000

    Update nomval.js

--- nomval.js
+++ nomval.js
@@ -4,22 +4,29 @@ const nom = document.getElementById('nom')
 function nomIsValid(name) {
     return /^[A-Z][a-zA-Z]+[\s]{0,1}[a-zA-Z]{0,10}$/.test(name);
 }
-function verification() {
+
+
+$("#Check").click(function(){
     if (nom.value == '') {
-        error.innerHTML = "name is required"
-        nom.className="form-control is-invalid"
+        $(error).text("name is required");
+        $(nom).removeClass("form-control is-valid");
+        $(nom).addClass("form-control is-invalid");
     }
     else if(nom.value.length < 4) {
-        error.innerHTML = "name is too short"
-        nom.className="form-control is-invalid"
+        $(error).text("name is too short");
+        $(nom).removeClass("form-control is-valid");
+        $(nom).addClass("form-control is-invalid");
     }
     else if(!nomIsValid(nom.value)) {
-            error.innerHTML = "insert a valid name"
-            nom.className="form-control is-invalid"
+            $(error).text("insert a valid name")
+            $(nom).removeClass("form-control is-valid");
+            $(nom).addClass("form-control is-invalid");
         }
     else
     {
-        nom.className="form-control is-valid"
-        error.innerHTML = ''
+        $(nom).removeClass("form-control is-invalid");
+        $(nom).addClass("form-control is-valid");
+        
+        $(error).text('');
     }
-}
+  });
